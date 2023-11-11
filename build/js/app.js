6e36a3b9a6697eac302d72547da23a627efbe0b7
Victor Flores <victor_2002fj@hotmail.com>
2023-11-11 15:45:34 -0600

    Agregado Login, Header para Index, Slider para cada producto

--- build/js/app.js
+++ build/js/app.js
@@ -1,2 +1,2 @@
-console.log("Desde consola");
+function eventListeners(){crearSliderCarrusel()}function crearSliderCarrusel(){const e=document.querySelectorAll(".carrusel-flecha-izquierda"),t=document.querySelectorAll(".carrusel-flecha-derecha");e.forEach(e=>{e.addEventListener("click",e=>{const t=e.target.parentNode.nextElementSibling;t.scrollLeft=t.scrollLeft-t.offsetWidth})}),t.forEach(e=>{e.addEventListener("click",e=>{const t=e.target.parentNode.previousElementSibling;t.scrollLeft=t.scrollLeft+t.offsetWidth})})}document.addEventListener("DOMContentLoaded",(function(){eventListeners()}));
 //# sourceMappingURL=app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
-console.log(`Desde consola`);
+document.addEventListener("DOMContentLoaded", function() {
+    eventListeners();
+});
+
+function eventListeners()
+{
+    crearSliderCarrusel();
+}
+
+function crearSliderCarrusel() {
+    const flechasIzquierdas = document.querySelectorAll(".carrusel-flecha-izquierda")
+    const flechasDerechas = document.querySelectorAll(".carrusel-flecha-derecha");
+    flechasIzquierdas.forEach(flecha_izquierda => {
+        flecha_izquierda.addEventListener("click", (e) => {
+            const fila_actual = e.target.parentNode.nextElementSibling;
+            fila_actual.scrollLeft = fila_actual.scrollLeft - fila_actual.offsetWidth;
+        });
+    });
+    flechasDerechas.forEach(flecha_derecha => {
+        flecha_derecha.addEventListener("click", (e) => {
+            const fila_actual = e.target.parentNode.previousElementSibling;
+            fila_actual.scrollLeft = fila_actual.scrollLeft + fila_actual.offsetWidth;
+        });
+    });
+}
